c454441f30fc3608ad418d948b97306abc9be59c
ThankUYou <2023282210229@whu.edu.cn>
2024-08-10 14:03:51 +0800

    lab1 map & reduce

--- src/mr/rpc.go
+++ src/mr/rpc.go
@@ -6,7 +6,10 @@ package mr
 // remember to capitalize all names.
 //
 
-import "os"
+import (
+	"os"
+	"time"
+)
 import "strconv"
 
 //
@@ -24,6 +27,45 @@ type ExampleReply struct {
 
 // Add your RPC definitions here.
 
+type TaskArgs struct {
+}
+
+type Task struct {
+	TaskId     int       // 任务唯一 id
+	TaskType   TaskType  // 任务类型 -> map/reduce
+	FileSlice  []string  // 输入的文件
+	ReducerNum int       // reducer 数量
+	State      TaskState // 任务状态
+	StartTime  time.Time // 任务开始时间
+}
+
+// TaskType 枚举
+type TaskType int
+
+const (
+	MapTask     TaskType = iota // Map
+	ReduceTask                  // Reduce
+	WaitingTask                 // 任务全部被分发完了
+	ExitTask                    // finish
+)
+
+// TaskState 枚举
+type TaskState int
+
+const (
+	Working TaskState = iota // 工作中
+	Waiting                  // 等待执行
+	Done                     // 已经完成
+)
+
+// Phase 程序所处阶段的枚举
+type Phase int
+
+const (
+	MapPhase    Phase = iota // map 阶段
+	ReducePhase              // reduce 阶段
+	AllDone                  // 已完成
+)
 
 // Cook up a unique-ish UNIX-domain socket name
 // in /var/tmp, for the coordinator.
--- src/mr/worker.go
+++ src/mr/worker.go
@@ -1,48 +1,171 @@
 package mr
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"sort"
+	"strconv"
+	"time"
+)
 import "log"
 import "net/rpc"
 import "hash/fnv"
 
-
-//
 // Map functions return a slice of KeyValue.
-//
 type KeyValue struct {
 	Key   string
 	Value string
 }
 
-//
+// SortedKey 有序键值对数组
+type SortedKey []KeyValue
+
+// Len 重写len,swap,less才能排序
+func (k SortedKey) Len() int           { return len(k) }
+func (k SortedKey) Swap(i, j int)      { k[i], k[j] = k[j], k[i] }
+func (k SortedKey) Less(i, j int) bool { return k[i].Key < k[j].Key }
+
 // use ihash(key) % NReduce to choose the reduce
 // task number for each KeyValue emitted by Map.
-//
 func ihash(key string) int {
 	h := fnv.New32a()
 	h.Write([]byte(key))
 	return int(h.Sum32() & 0x7fffffff)
 }
 
-
-//
 // main/mrworker.go calls this function.
-//
 func Worker(mapf func(string, string) []KeyValue,
 	reducef func(string, []string) string) {
 
 	// Your worker implementation here.
-
-	// uncomment to send the Example RPC to the coordinator.
-	// CallExample()
-
+	flag := true
+	for flag {
+		// send an RPC to the coordinator asking for a task
+		task := getTask()
+		switch task.TaskType {
+		case MapTask:
+			{
+				DoMapTask(mapf, &task)
+				callDone(&task)
+			}
+		case ReduceTask:
+			{
+				DoReduceTask(reducef, &task)
+				callDone(&task)
+			}
+		case WaitingTask:
+			{
+				fmt.Println("[INFO] All tasks are in progress, please wait")
+				time.Sleep(time.Second)
+			}
+		case ExitTask:
+			{
+				fmt.Println("[INFO] exit")
+				flag = false
+			}
+		}
+	}
 }
 
-//
+func getTask() Task {
+	reqArgs := TaskArgs{}
+	replyTask := Task{}
+	ok := call("Coordinator.PollTask", &reqArgs, &replyTask)
+	if !ok {
+		fmt.Printf("[ERROR] call failed\n")
+	}
+	return replyTask
+}
+
+func DoMapTask(mapf func(string, string) []KeyValue, response *Task) {
+	var intermediate []KeyValue
+	filename := response.FileSlice[0]
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatalf("[ERROR] cannot open %v", filename)
+	}
+	// 通过io工具包获取conten,作为mapf的参数
+	content, err := ioutil.ReadAll(file)
+	if err != nil {
+		log.Fatalf("[ERROR] cannot read %v", filename)
+	}
+	file.Close()
+	// map返回一组KV结构体数组
+	intermediate = mapf(filename, string(content))
+	//initialize and loop over []KeyValue
+	rn := response.ReducerNum
+	// 创建一个长度为nReduce的二维切片
+	HashedKV := make([][]KeyValue, rn)
+	for _, kv := range intermediate {
+		HashedKV[ihash(kv.Key)%rn] = append(HashedKV[ihash(kv.Key)%rn], kv)
+	}
+	for i := 0; i < rn; i++ {
+		oname := "mr-tmp-" + strconv.Itoa(response.TaskId) + "-" + strconv.Itoa(i)
+		ofile, _ := os.Create(oname)
+		enc := json.NewEncoder(ofile)
+		for _, kv := range HashedKV[i] {
+			err := enc.Encode(kv)
+			if err != nil {
+				return
+			}
+		}
+		ofile.Close()
+	}
+}
+
+func DoReduceTask(reducef func(string, []string) string, response *Task) {
+	reduceFileNum := response.TaskId
+	intermediate := shuffle(response.FileSlice)
+	dir, _ := os.Getwd()
+	tempFile, err := ioutil.TempFile(dir, "mr-tmp-*")
+	if err != nil {
+		log.Fatal("[ERROR] Failed to create temp file", err)
+	}
+	i := 0
+	for i < len(intermediate) {
+		j := i + 1
+		for j < len(intermediate) && intermediate[j].Key == intermediate[i].Key {
+			j++
+		}
+		var values []string
+		for k := i; k < j; k++ {
+			values = append(values, intermediate[k].Value)
+		}
+		output := reducef(intermediate[i].Key, values)
+		fmt.Fprintf(tempFile, "%v %v\n", intermediate[i].Key, output)
+		i = j
+	}
+	tempFile.Close()
+
+	// 在完全写入后进行重命名
+	fn := fmt.Sprintf("mr-out-%d", reduceFileNum)
+	os.Rename(tempFile.Name(), fn)
+}
+
+// 传入文件名数组,返回排序好的 kv 数组
+func shuffle(files []string) []KeyValue {
+	var kva []KeyValue
+	for _, filepath := range files {
+		file, _ := os.Open(filepath)
+		dec := json.NewDecoder(file)
+		for {
+			var kv KeyValue
+			if err := dec.Decode(&kv); err != nil {
+				break
+			}
+			kva = append(kva, kv)
+		}
+		file.Close()
+	}
+	sort.Sort(SortedKey(kva))
+	return kva
+}
+
 // example function to show how to make an RPC call to the coordinator.
 //
 // the RPC argument and reply types are defined in rpc.go.
-//
 func CallExample() {
 
 	// declare an argument structure.
@@ -61,11 +184,9 @@ func CallExample() {
 	fmt.Printf("reply.Y %v\n", reply.Y)
 }
 
-//
 // send an RPC request to the coordinator, wait for the response.
 // usually returns true.
 // returns false if something goes wrong.
-//
 func call(rpcname string, args interface{}, reply interface{}) bool {
 	// c, err := rpc.DialHTTP("tcp", "127.0.0.1"+":1234")
 	sockname := coordinatorSock()
@@ -82,4 +203,19 @@ func call(rpcname string, args interface{}, reply interface{}) bool {
 
 	fmt.Println(err)
 	return false
+}
+
+// callDone Call RPC to mark the task as completed
+func callDone(finishedTask *Task) Task {
+
+	reply := Task{}
+	ok := call("Coordinator.SetTaskDone", finishedTask, &reply)
+
+	if ok {
+		fmt.Println("[INFO] close task: ", finishedTask)
+	} else {
+		fmt.Printf("call failed!\n")
+	}
+	return reply
+
 }
